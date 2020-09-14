cc903b9163860609b39f89934bac529551e9c86f
jhonny689 <jhonny.chamoun@gmail.com>
2020-09-14 15:46:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function moveDodgerLeft(){
+    let left_val = parseInt(dodger.style.left);
+    debugger;
+    if (left_val > 1){
+        dodger.style.left = `${--left_val}px`;
+        console.log(dodger.style.left);
+    }
+};
+
+let moveDodgerRight = ()=>{
+    let left_val = parseInt(dodger.style.left);
+    debugger;
+    if (left_val < 359){
+        dodger.style.left = `${++left_val}px`;
+        console.log(dodger.style.left);
+    }
+};
